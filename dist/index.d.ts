d4fd3b20d4a5b5b44d2d80cfce45f14db59523c0
Jack Le Hamster <jacklehamster@gmail.com>
2022-12-06 07:41:58 -0800

    cccc

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,14 @@
-declare function recursePath(path: string, file: File): void;
-declare const exports: {
-    recursePath: typeof recursePath;
-};
-export default exports;
+interface Props {
+    ignore?: string[];
+    cutoff?: number;
+    space?: string;
+}
+interface DirectoryStructure {
+    md5?: string;
+    dir?: Record<string, DirectoryStructure>;
+    files?: Record<string, number>;
+}
+export declare function recursePath(path: string, callback: (filepath: string) => Promise<void>, options: Props): Promise<unknown>;
+export declare function getDirectoryStructure(path: string, { ignore, cutoff }: Props): Promise<DirectoryStructure>;
+export declare function saveDirectoryStructure(path: string, target: string, { ignore, cutoff, space }: Props): Promise<void>;
+export {};
--- dist/index.test.js
+++ dist/index.test.js
@@ -1,12 +1,10 @@
-import { saveDirectoryStructure } from "./index"
-import fs from "fs"
-
-
+import { saveDirectoryStructure } from "./index";
+import fs from "fs";
 saveDirectoryStructure(".", "dir.json", { ignore: ['./.git', './node_modules'], cutoff: 1, space: "  " })
-  .then(() => {
+    .then(() => {
     const content = fs.readFileSync("dir.json", { encoding: "utf8" });
     console.info(content);
-  })
-  .then(() => {
+})
+    .then(() => {
     fs.unlinkSync("dir.json");
-  });
+});
